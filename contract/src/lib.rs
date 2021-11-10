7d8332a05c9e95533a25829d8b358a9d2abeffd8
Miraculous Owonubi <omiraculous@gmail.com>
2021-11-10 02:14:32 +0100

    temporarily drop floats for u8, endure lossiness

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -13,7 +13,7 @@ pub struct Votes {
 #[derive(Debug, PartialEq, Serialize, BorshSerialize, BorshDeserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct UserState {
-    rating: f32,
+    rating: u8,
     #[serde(flatten)]
     votes: Votes,
 }
@@ -21,7 +21,7 @@ pub struct UserState {
 impl Default for UserState {
     fn default() -> Self {
         Self {
-            rating: 2.0, // you're a 2 simply for existing
+            rating: 20, // you're a 2 ⭐️ simply for existing
             votes: Votes {
                 given: 0,
                 received: 1,
@@ -44,9 +44,8 @@ impl Default for NoseDive {
     }
 }
 
-fn validate_rating(rating: f32) -> bool {
-    let fract = rating.fract();
-    (rating >= 0.0 && rating <= 5.0) && (fract == 0.0 || fract == 0.5)
+fn validate_rating(rating: u8) -> bool {
+    matches!((rating, rating % 5), (0..=50, 0))
 }
 
 #[near_bindgen]
@@ -65,10 +64,10 @@ impl NoseDive {
         self.records.get(&account_id)
     }
 
-    pub fn vote_for(&mut self, account_id: AccountId, rating: f32) {
+    pub fn vote_for(&mut self, account_id: AccountId, rating: u8) {
         require!(
             validate_rating(rating),
-            "enter a valid rating: multiples of 0.5 between 0 and 5"
+            "enter a valid rating: multiples of 5 between 0 and 50"
         );
         let your_account_id = env::signer_account_id();
         let mut your_state = self.records.get(&your_account_id).expect(&format!(
@@ -80,13 +79,13 @@ impl NoseDive {
             account_id,
         ));
         require!(account_id != your_account_id, "you can't rate yourself");
-        their_state.rating = ((their_state.rating * their_state.votes.received as f32)
-            + (rating + your_state.rating) / 2.0)
+        their_state.rating = (((their_state.rating as u64 * their_state.votes.received)
+            + (rating + your_state.rating) as u64 / 2)
             / {
                 your_state.votes.given += 1;
                 their_state.votes.received += 1;
-                their_state.votes.received as f32
-            };
+                their_state.votes.received
+            }) as u8;
         self.records.insert(&your_account_id, &your_state);
         self.records.insert(&account_id, &their_state);
     }
@@ -129,7 +128,7 @@ mod tests {
         assert_eq!(
             get_stats_for(alice()),
             Some(UserState {
-                rating: 2.0,
+                rating: 20,
                 votes: Votes {
                     given: 0,
                     received: 1,
@@ -139,7 +138,7 @@ mod tests {
         assert_eq!(
             get_stats_for(bob()),
             Some(UserState {
-                rating: 2.0,
+                rating: 20,
                 votes: Votes {
                     given: 0,
                     received: 1,
@@ -154,14 +153,14 @@ mod tests {
         stage(bob()).register();
         // --
         for _ in 1..=10 {
-            stage(bob()).vote_for(alice(), 4.5);
-            stage(alice()).vote_for(bob(), 5.0);
+            stage(bob()).vote_for(alice(), 45);
+            stage(alice()).vote_for(bob(), 50);
         }
         // --
         assert_eq!(
             get_stats_for(alice()),
             Some(UserState {
-                rating: 3.7977424,
+                rating: 34,
                 votes: Votes {
                     given: 10,
                     received: 11,
@@ -171,7 +170,7 @@ mod tests {
         assert_eq!(
             get_stats_for(bob()),
             Some(UserState {
-                rating: 4.006109,
+                rating: 36,
                 votes: Votes {
                     given: 10,
                     received: 11,
@@ -182,26 +181,14 @@ mod tests {
 
     #[test]
     fn validate_5_star_as_fract() {
-        for rating in [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0] {
+        for rating in [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50] {
             assert!(
                 validate_rating(rating),
                 "valid rating specification marked invalid: {:.1}",
                 rating
             );
         }
-        for rating in [
-            f32::NEG_INFINITY,
-            f32::NAN,
-            -2.5,
-            -0.1,
-            0.1,
-            0.2,
-            0.3,
-            0.4,
-            5.5,
-            10.0,
-            f32::INFINITY,
-        ] {
+        for rating in [1, 2, 3, 4, 34, 55, 100] {
             assert!(
                 !validate_rating(rating),
                 "invalid rating specification marked valid: {:.1}",
